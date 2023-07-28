054bf477b551bd3beeab4402eeea2a24841c966f
iwilare <iwilare@gmail.com>
2023-07-29 00:25:19 +0300

    Remove debug

--- home.nix
+++ home.nix
@@ -13,9 +13,8 @@ let username = "andrea";
       shellInit =
         ''
           ${prev.shellInit}
-          echo "CIAOSSO"
           if test -z (pgrep ssh-agent)
-            eval (ssh-agent -c)
+            eval (ssh-agent -c) > /dev/null
             set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
             set -Ux SSH_AGENT_PID $SSH_AGENT_PID
           end
