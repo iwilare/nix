0fed5e3fc9615b971165958692a63ac1f696549b
iwilare <iwilare@gmail.com>
2024-06-23 13:39:25 +0300

    2024-06-23 13:39:25

--- home/home.nix
+++ home/home.nix
@@ -100,7 +100,18 @@
       nos = "sudo nixos-rebuild switch";
       hm  = "cd ~/.config/home-manager; code .";
       hmd = "cd ~/.config/home-manager";
-      hms = "home-manager switch -b backup --flake .#${if !pkgs.stdenv.isDarwin then "andrea" else "andrea-macos"}";
+      hms = "home-manager switch -b backup --flake ~/.config/home-manager#${if !pkgs.stdenv.isDarwin then "andrea" else "andrea-macos"}";
     };
+    plugins = [
+      {
+        name = "nix-env.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+        };
+      }
+    ];
   };
 }
