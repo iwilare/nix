3a9751bf84705d34939ed25cc665ca77da875d54
Andrea Laretto <iwilare@gmail.com>
2025-10-30 22:56:34 +0200

    Revise NixOS setup instructions for WSL

--- nix.sh
+++ nix.sh
@@ -1,16 +1,19 @@
-# A simple guide to set up NixOS on WSL
+# A zero-thoughts guide to set up NixOS on WSL
 
 wsl --install -d Debian
-wsl
 sudo apt update
-sudo apt install wget -y
+sudo apt install wget git -y
 code .
-# at this point: clone iwilare/ssh from vscode with auth
-chmod 600 ~/.ssh/id_ed25519
+cd ~
+explorer.exe .
+# at this point: unzip in .ssh the files from https://github.com/iwilare/.ssh/archive/refs/heads/main.zip
+sudo chown -R andrea .ssh
+sudo chmod 600 ~/.ssh/id_ed25519
 sudo apt install curl xz-utils -y
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon # https://nixos.org/download/
 . /home/andrea/.nix-profile/etc/profile.d/nix.sh
 git clone git@github.com:iwilare/nix .config/home-manager
+sudo apt remove git
 mkdir ~/.config/nix
 echo 'experimental-features = nix-command flakes' > ~/.config/nix/nix.conf
 nix run nixpkgs#home-manager -- switch -b backup
