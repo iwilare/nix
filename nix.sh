cdc77ad5d9dfc5a4d8ede03cd8e4eb594b7ce1f6
Andrea Laretto <iwilare@gmail.com>
2025-11-04 01:39:06 +0200

    Last change to apt remove -y

--- nix.sh
+++ nix.sh
@@ -12,11 +12,12 @@ sudo apt install curl xz-utils -y
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon # https://nixos.org/download/
 . /home/andrea/.nix-profile/etc/profile.d/nix.sh
 git clone git@github.com:iwilare/nix .config/home-manager
-sudo apt remove git
+sudo apt remove git -y
 mkdir ~/.config/nix
 echo 'experimental-features = nix-command flakes' > ~/.config/nix/nix.conf
 nix run nixpkgs#home-manager -- switch -b backup
 echo '/home/andrea/.nix-profile/bin/fish' | sudo tee -a /etc/shells > /dev/null
 chsh -s /home/andrea/.nix-profile/bin/fish
 ln -s /mnt/c/Dropbox ~
+
 
