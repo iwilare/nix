a5c9ffad49f305d6f5706ad258a995fcc7dfb6e1
iwilare <iwilare@gmail.com>
2026-05-27 15:50:19 +0300

    Remove dropbox

--- nixos/system.nix
+++ nixos/system.nix
@@ -5,25 +5,20 @@
   home-manager.backupFileExtension = "backup";
 
   environment.systemPackages = with pkgs; [
-    dropbox
-    spotify
-    telegram-desktop
+    # dropbox
+    # spotify
 
+    telegram-desktop
     discord
     google-chrome
     teamviewer
 
-    pcmanfm
-
-    rofi
     pavucontrol
-
+    pcmanfm
+    rofi
     lxrandr
     arandr
-
     lxappearance
-
-    #obsidian
 
     #mate.engrampa
 
@@ -190,25 +185,25 @@
   # ------------ services ------------
 
   services.udev.packages = [ pkgs.gnome-settings-daemon ];
-  networking.firewall = {
-    allowedTCPPorts = [ 17500 ];
-    allowedUDPPorts = [ 17500 ];
-  };
-  systemd.user.services.dropbox = {
-    description = "Dropbox";
-    wantedBy = [ "graphical-session.target" ];
-    environment = {
-      QT_PLUGIN_PATH = "/run/current-system/sw/${pkgs.qt5.qtbase.qtPluginPrefix}";
-      QML2_IMPORT_PATH = "/run/current-system/sw/${pkgs.qt5.qtbase.qtQmlPrefix}";
-    };
-    serviceConfig = {
-      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
-      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
-      KillMode = "control-group"; # upstream recommends process
-      Restart = "on-failure";
-      PrivateTmp = true;
-      ProtectSystem = "full";
-      Nice = 10;
-    };
-  };
+  # networking.firewall = {
+  #   allowedTCPPorts = [ 17500 ];
+  #   allowedUDPPorts = [ 17500 ];
+  # };
+  # systemd.user.services.dropbox = {
+  #   description = "Dropbox";
+  #   wantedBy = [ "graphical-session.target" ];
+  #   environment = {
+  #     QT_PLUGIN_PATH = "/run/current-system/sw/${pkgs.qt5.qtbase.qtPluginPrefix}";
+  #     QML2_IMPORT_PATH = "/run/current-system/sw/${pkgs.qt5.qtbase.qtQmlPrefix}";
+  #   };
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+  #     ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+  #     KillMode = "control-group"; # upstream recommends process
+  #     Restart = "on-failure";
+  #     PrivateTmp = true;
+  #     ProtectSystem = "full";
+  #     Nice = 10;
+  #   };
+  # };
 }
